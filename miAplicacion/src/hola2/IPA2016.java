95dc5eed104f60d54c8874acdd9ec0d16cf08617
PathyMG <pathy.marquez.72@hotmail.com>
2016-11-01 15:21:35 -0600

    Mi primera aplicación en Java en Intelli

---
+++ miAplicacion/src/hola2/IPA2016.java
@@ -0,0 +1,11 @@
+package hola2;
+
+/**
+ * Created by pTHY on 02/11/2016.
+ */
+public class IPA2016 {
+
+    public static void main(String[] args) {
+        System.out.println("Hola soy un app");
+    }
+}
