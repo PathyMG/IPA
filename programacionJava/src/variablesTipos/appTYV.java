8ad8394011c373b1b65a58f9d7f06ee0294fed07
PathyMG <pathy.marquez.72@hotmail.com>
2016-11-02 12:26:55 -0600

    Mi segunda aplicación para tipos de datos

---
+++ programacionJava/src/variablesTipos/appTYV.java
@@ -0,0 +1,33 @@
+package variablesTipos;
+
+/**
+ * Created by pTHY on 02/11/2016.
+ */
+public class appTYV {
+
+    public static void main(String[] args) {
+
+        //Tipo de constante.
+        final int dias = 9821;
+
+        //Tipo de datos.
+        int entero;
+        boolean elige;
+        String soyUnaPalabra;
+        float soyDecimal;
+        double soyMuyPreciso;
+        char soyUnCaracter;
+        entero = 515;
+        elige = true;
+        soyUnaPalabra = "Todos somos muy dedicados";
+        soyDecimal = 14.94f;
+        soyMuyPreciso = 145.146;
+        soyUnCaracter = 'a';
+
+        int creado2016 = 3123891;
+
+        System.out.println(dias + entero + soyMuyPreciso + soyUnaPalabra + soyDecimal + soyUnCaracter + " son algunas de las variables que se pueden presentar aqui " + elige);
+
+
+    }
+}
